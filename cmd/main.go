4b81f1be809530a79eb910c5869e364a043c7f08
MrTuxx <fran_mont_gonz@hotmail.com>
2024-05-22 13:11:56 +0200

    Implement AES256 encryption example with password retrieval via URL and Brainfuck encoding for regex and URL

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/MrTuxx/OffensiveGolang/pkg/encryption"
+	"github.com/MrTuxx/OffensiveGolang/pkg/evasion"
 )
 
 func main() {
 
-	encryption.GetEncryption("<SHELLCODE>")
+	//encryption.GetEncryption("<SHELLCODE>")
+
+	url := "<URL>"
+	pattern := `<REGEX TO GET THE PASSWORD IN THE URL>`
+	password, err := evasion.ExtractMatchedStringFromURL(url, pattern)
+	if err != nil {
+		fmt.Println("[!] Error Extracting the String")
+	}
+
+	println("[+] Extracted Password: ", password)
+
+	encryption.GetEncryptionWithPassword("<SHELLCODE>", password)
+
+	println("[+] URL Brainfuck encoded: ", evasion.CodeBrainfuck(url))
+	println("[+] Pattern Brainfuck encoded: ", evasion.CodeBrainfuck(pattern))
 
 }
